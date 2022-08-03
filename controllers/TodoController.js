5d193a3eefc48d55fffb404c5c68abce9d16ea6d
Dinuka <dinukabk@gmail.com>
2022-08-03 23:41:40 +0530

    update

---
+++ controllers/TodoController.js
@@ -0,0 +1,24 @@
+const ToDoModel = require("../models/ToDoModal");
+
+module.exports.getToDo = async (req, res) => {
+    const todo = await ToDoModel.find();
+    res.send(todo);
+}
+
+module.exports.saveToDo = (req, res) => {
+    const { text } = req.body;
+
+    ToDoModel
+        .create({ text })
+        .then(() => res.set(201).send("Added Successfully..."))
+        .catch((err) => console.log(err));
+}
+
+module.exports.deleteToDo = (req, res) => {
+    const { _id } = req.body;
+
+    ToDoModel
+        .findByIdAndDelete(_id)
+        .then(() => res.set(201).send("Deleted Successfully..."))
+        .catch((err) => console.log(err));
+}
